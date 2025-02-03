c85f92491f9fa30141ec14c3bcf28c39e0853059
junbao <2438229875@qq.com>
2025-02-03 22:36:37 +0800

    feat: 实现nacos配置中心

--- yvbao-live-user-provider/src/main/java/cn/junbao/yvbao/live/user/provider/rpc/UserRpcImpl.java
+++ yvbao-live-user-provider/src/main/java/cn/junbao/yvbao/live/user/provider/rpc/UserRpcImpl.java
@@ -1,10 +1,8 @@
 package cn.junbao.yvbao.live.user.provider.rpc;
 
-import cn.junbao.yvbao.live.common.interfaces.utils.ConvertBeanUtils;
 import cn.junbao.yvbao.live.user.dto.UserDTO;
 import cn.junbao.yvbao.live.user.interfaces.IUserRpc;
 import cn.junbao.yvbao.live.user.provider.service.IUserService;
-import groovy.util.logging.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +10,6 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
 
 @DubboService
 public class UserRpcImpl implements IUserRpc {
