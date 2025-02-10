bff837b3dd7467a50c52d6706786bfd951e4452b
junbao <2438229875@qq.com>
2025-02-10 17:00:25 +0800

    feat: 登录注册功能接口

--- yubao-live-framework/yubao-live-framework-redis-starter/src/main/java/cn/junbao/yubao/live/framework/redis/starter/config/RedisConfig.java
+++ yubao-live-framework/yubao-live-framework-redis-starter/src/main/java/cn/junbao/yubao/live/framework/redis/starter/config/RedisConfig.java
@@ -2,17 +2,6 @@
 package cn.junbao.yubao.live.framework.redis.starter.config;
 
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.ByteBufOutputStream;
-import org.redisson.client.codec.BaseCodec;
-import org.redisson.client.protocol.Decoder;
-import org.redisson.client.protocol.Encoder;
-import org.redisson.codec.JsonJacksonCodec;
-import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,8 +9,6 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.io.IOException;
 
 @Configuration
 @ConditionalOnClass(RedisTemplate.class)
@@ -46,34 +33,4 @@ public class RedisConfig {
     }
 
 
-    static class RedisCodec extends BaseCodec {
-
-        private final Encoder encoder = in -> {
-            ByteBuf out = ByteBufAllocator.DEFAULT.buffer();
-            try {
-                ByteBufOutputStream os = new ByteBufOutputStream(out);
-                JSON.writeJSONString(os, in, SerializerFeature.WriteClassName);
-                return os.buffer();
-            } catch (IOException e) {
-                out.release();
-                throw e;
-            } catch (Exception e) {
-                out.release();
-                throw new IOException(e);
-            }
-        };
-
-        private final Decoder<Object> decoder = (buf, state) -> JSON.parseObject(new ByteBufInputStream(buf), Object.class);
-
-        @Override
-        public Decoder<Object> getValueDecoder() {
-            return decoder;
-        }
-
-        @Override
-        public Encoder getValueEncoder() {
-            return encoder;
-        }
-
-    }
 }
